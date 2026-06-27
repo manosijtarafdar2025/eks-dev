68438c2f8489ea426d081f41b8fae29d95ab5495
Manosij Tarafdar <manosijtarafdar.2025@gmail.com>
2026-06-27 23:00:34 +0530

    Creating EKS cluster - add subnet

--- eks.tf
+++ eks.tf
@@ -1,9 +1,9 @@
 module "eks-dev" {
-  source                   = "./modules/aws-eks"
-  create                   = var.create
-  name                     = var.name
-  kubernetes_version       = var.kubernetes_version
-  environment              = var.environment
-  tags                     = var.tags
-  subnet_ids               = ["${aws_subnet.eks_private_subnet}"]
+  source             = "./modules/aws-eks"
+  create             = var.create
+  name               = var.name
+  kubernetes_version = var.kubernetes_version
+  environment        = var.environment
+  subnet_ids         = [aws_subnet.eks_private_subnet.id]
+  tags               = var.tags
 }
