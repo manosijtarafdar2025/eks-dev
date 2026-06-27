e7cbc318c0ead220bc5b347cfc079cc52f7b11f3
Manosij Tarafdar <manosijtarafdar.2025@gmail.com>
2026-06-27 20:45:25 +0530

    Initial Commit

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  cloud {
+    organization = "manosij-prd"
+    workspaces {
+      name = "eks-dev"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
