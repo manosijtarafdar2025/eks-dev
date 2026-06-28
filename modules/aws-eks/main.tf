6664c026c5d7afc73f3e7dad75ca1e5b33f88a11
Manosij Tarafdar <manosijtarafdar.2025@gmail.com>
2026-06-28 12:06:57 +0530

    Creating EKS cluster - add sg and fix vpc

--- modules/aws-eks/main.tf
+++ modules/aws-eks/main.tf
@@ -7,5 +7,7 @@ module "eks" {
   kubernetes_version       = var.kubernetes_version
   # Subnet Configuration
   subnet_ids               = var.subnet_ids
+  # Security Group Configuration
+  security_group_id        = var.cluster_security_group
   tags                     = var.tags
 }
